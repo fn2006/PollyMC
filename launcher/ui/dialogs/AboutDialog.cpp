fd36089aa985d939fa41e7c23e9b9a32277468bd
Fintan Martin <fintanmartin6@gmail.com>
2022-07-18 21:00:54 +0100

    Add myself and other contributors to AboutDialog.cpp

--- launcher/ui/dialogs/AboutDialog.cpp
+++ launcher/ui/dialogs/AboutDialog.cpp
@@ -71,6 +71,16 @@ QString getCreditsHtml()
 
     //: %1 is the name of the launcher, determined at build time, e.g. "PolyMC Developers"
     stream << "<h3>" << QObject::tr("%1 Developers", "About Credits").arg(BuildConfig.LAUNCHER_NAME) << "</h3>\n";
+    stream << QString("<p>fn2006 %1</p>\n")                     .arg(getGitHub("fn2006"));
+    stream << "<br />\n";
+
+    //: %1 is the name of the launcher, determined at build time, e.g. "PolyMC Contributors"
+    stream << "<h3>" << QObject::tr("%1 Contributors", "About Credits").arg(BuildConfig.LAUNCHER_NAME) << "</h3>\n";
+    stream << QString("<p>anoraktrend %1</p>\n")                .arg(getGitHub("anoraktrend"));
+    stream << "<br />\n";
+
+    //: %1 is the name of the launcher, determined at build time, e.g. "PolyMC Developers"
+    stream << "<h3>" << QObject::tr("%1 Developers", "About Credits").arg("PolyMC") << "</h3>\n";
     stream << QString("<p>LennyMcLennington %1</p>\n")          .arg(getGitHub("LennyMcLennington"));
     stream << QString("<p>Sefa Eyeoglu (Scrumplex) %1</p>\n")   .arg(getWebsite("https://scrumplex.net"));
     stream << QString("<p>dada513 %1</p>\n")                    .arg(getGitHub("dada513"));
@@ -81,7 +91,7 @@ QString getCreditsHtml()
     stream << "<br />\n";
 
     //: %1 is the name of the launcher, determined at build time, e.g. "PolyMC Contributors"
-    stream << "<h3>" << QObject::tr("%1 Contributors", "About Credits").arg(BuildConfig.LAUNCHER_NAME) << "</h3>\n";
+    stream << "<h3>" << QObject::tr("%1 Contributors", "About Credits").arg("PolyMC") << "</h3>\n";
     stream << QString("<p>DioEgizio %1</p>\n")                      .arg(getGitHub("DioEgizio"));
     stream << QString("<p>flowln %1</p>\n")                      .arg(getGitHub("flowln"));
     stream << QString("<p>swirl %1</p>\n")                      .arg(getWebsite("https://swurl.xyz/"));
